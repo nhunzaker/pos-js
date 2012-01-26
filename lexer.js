2f5fb8e44de1a70a4b7f8240e0a2456f9b4d5845
Nathan Hunzaker <nate.hunzaker@gmail.com>
2012-01-26 06:56:05 -0500

    another fix

--- lexer.js
+++ lexer.js
@@ -6,8 +6,6 @@
  * http://www.opensource.org/licenses/lgpl-3.0.html
  */
 
-module.exports = Lexer;
-
 var re = {
     // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
     url: /\b(?:(?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig,
@@ -18,10 +16,10 @@ var re = {
     file: /\S+\.\S+[^\/\?]/ig
 };
 
-function Lexer(){
+var Lexer = module.exports = function (){
     // Split by urls, then numbers, then whitespace, then punctuation
     this.regexs = [re.url, re.file, re.number, re.space, re.punctuation];
-}
+};
 
 function LexerNode(string, regex, regexs){
 
